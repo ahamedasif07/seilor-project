1172ecff35937edff76ed7dbe1e41bccc333da6a
rxasif07 <rxasif31@gmail.com>
2025-07-05 14:08:17 +0600

    women drop dowen done

--- src/components/NavBar/BottomNav.jsx
+++ src/components/NavBar/BottomNav.jsx
@@ -371,7 +371,91 @@ const BottomNav = () => {
             </div>
           </div>
           {/* mens dropdowen end */}
-          <h3 className="text-[15px] font-semibold text-gray-700">WOMEN</h3>
+
+          {/* women start dropdowen start*/}
+          <div className="relative group   text-center cursor-pointer transition-all duration-200  border-orange-400 hover:border-[#91d36b]  text-white group">
+            {/* Trigger */}
+            <div className="relative group">
+              {/* Trigger */}
+              <h3 className="text-[15px] font-semibold text-gray-700 cursor-pointer">
+                WOMEN'S
+              </h3>
+
+              {/* Dropdown */}
+              <div
+                className={`absolute left-5  transform -translate-x-1/2 z-[1000] px-[10px] w-[99vw] top-full py-2  rounded-sm mt-[7px] bg-white text-gray-400 shadow-lg
+    duration-300 flex flex-col items-start 
+    opacity-0 invisible group-hover:opacity-100 group-hover:visible group-hover:translate-y-2 transition-all`}
+              >
+                <div className="mx-auto w-full px-2 flex flex-col md:flex-row justify-between gap-6 items-start">
+                  {/* Left image */}
+                  <div className="w-full   md:w-1/4 shrink-0">
+                    <div className="relative w-2/3 h-[300px] lg:h-[400px]">
+                      <Image
+                        src={menDpImgOne}
+                        alt="Left"
+                        layout="fill"
+                        objectFit="cover"
+                        priority
+                      />
+                    </div>
+                  </div>
+
+                  {/* Grid content */}
+                  <div className="grid grid-cols-2 md:grid-cols-3 gap-6 w-full md:w-2/4 text-[14px] text-gray-700">
+                    <div>
+                      <h4 className="font-semibold mb-2 text-left">TOP</h4>
+                      {/* shirts */}
+                      <h4 className="font-semibold my-2 text-left">SHIRT</h4>
+                      <ul className="space-y-1">
+                        <li className="text-left text-[12px]">CROP TOP</li>
+                        <li className="text-left text-[12px]">SHORT TOP</li>
+                        <li className="text-left text-[12px]">LONG TOP</li>
+                      </ul>
+                    </div>
+                    <div>
+                      <h4 className="font-semibold mb-2 text-left">KAFTAN</h4>
+                      {/* polos */}
+                      <h4 className="font-semibold mb-2 text-left">2 PEC</h4>
+                      <h4 className="font-semibold mb-2 text-left">SHIRT</h4>
+                      <h4 className="font-semibold mb-2 text-left">
+                        SALOWER SUIT
+                      </h4>
+                    </div>
+                    <div>
+                      <h4 className="font-semibold mb-2 text-left">BOOTOM</h4>
+                      <ul className="space-y-1">
+                        <li className="text-left text-[12px]">SKIRT</li>
+                      </ul>
+                    </div>
+                  </div>
+
+                  {/* Right image */}
+                  <div className="w-full md:w-1/4 flex gap-3 shrink-0">
+                    <div className="relative w-1/2 h-[200px] lg:h-[300px]">
+                      <Image
+                        src={menDpImgTwo}
+                        alt="Right 1"
+                        layout="fill"
+                        objectFit="cover"
+                        priority
+                      />
+                    </div>
+                    <div className="relative w-1/2 h-[200px]  lg:h-[300px]">
+                      <Image
+                        src={menDpImgTheree}
+                        alt="Right 2"
+                        layout="fill"
+                        objectFit="cover"
+                        priority
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* women dropdowen end */}
           <h3 className="text-[15px] font-semibold text-gray-700">KID'S</h3>
           <h3 className="text-[15px] font-semibold text-gray-700">NEWBORN</h3>
           <h3 className="text-[15px] font-semibold text-gray-700">FOOTWEAR</h3>
